d5498dba344dcb3e7d8e310fb5bbd4434fa13cbe
root <root@debian-m25>
2015-03-16 14:29:39 +0300

    new file:   trunk/kernel/config-m25-1.0.2.0 	modified:   un_tar_all.sh

--- un_tar_all.sh
+++ un_tar_all.sh
@@ -2,12 +2,12 @@
 
 cd toolchain
 tar xfapv gcc-4.2.tar.bz2
-tar xfapv gcc.tar.bz2
-tar xfapv glibc-2.5.tar.bz2
+#tar xfapv gcc.tar.bz2
+#tar xfapv glibc-2.5.tar.bz2
 
 cd ../trunk/bootcode
-tar xfapv u-boot-3.4.9.tar.bz2
-tar xfapv u-boot-3.5.9.tar.bz2
+#tar xfapv u-boot-3.4.9.tar.bz2
+#tar xfapv u-boot-3.5.9.tar.bz2
 tar xfapv u-boot-3.6.9.tar.bz2
 
 cd ../image
